634dbf284d78410dce13953efe660e18867c4a3a
bryan leighton <bryan.leigh77@gmail.com>
2021-01-08 22:01:31 -0800

    api

--- 02-WEB-APIs/First_API_Django/onlinestore/products/urls.py
+++ 02-WEB-APIs/First_API_Django/onlinestore/products/urls.py
@@ -5,7 +5,4 @@ from .views import (manufacturer_detail, manufacturer_list,
 urlpatterns = [
     path("products/", product_list, name="product-list"),
     path("products/<int:pk>/", product_detail, name="product-detail"),
-
-    path("manufacturers/", manufacturer_list, name="manufacturer-list"),
-    path("manufacturers/<int:pk>/", manufacturer_detail, name="manufacturer-detail")
 ]
